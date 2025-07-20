cacc9e7c6982c8a2ab0287304da3bd5ee1c6fe27
pokornysimon <salamon.sima@gmail.com>
2025-07-20 13:13:56 +0200

    Coloured kingdom table + make transaction button added

--- BlazorSignalRApp.Client/Models/Kingdom.cs
+++ BlazorSignalRApp.Client/Models/Kingdom.cs
@@ -2,14 +2,6 @@ namespace BlazorSignalRApp.Client.Models;
 
 public class Kingdom(string kingdomName)
 {
-    public static int ExRateLowAmount = 1;
-    public static int ExRateLow = 10;
-    public static int ExRateMedAmount = 2;
-    public static int ExRateMed = 20;
-    public static int ExRateHighAmount = 3;
-
-    public static int HomeResourceAmountFreeMoreThan = 30;
-    
     public string KingdomName { get; } = kingdomName;
 
     public List<Resource> Resources { get; set; }
@@ -20,25 +12,8 @@ public class Kingdom(string kingdomName)
         r.Amount += amount;
     }
 
-    public int GetExchangeAmount(string offeredResourceName, string targetResourceName)
+    public Resource GetHomeResource()
     {
-        var offered = Resources.FirstOrDefault(r => r.Name == offeredResourceName);
-        var target = Resources.FirstOrDefault(r => r.Name == targetResourceName);
-
-        var diff = target!.Amount - offered!.Amount;
-
-        int ret;
-        if (diff > Kingdom.ExRateMed)
-            ret = Kingdom.ExRateHighAmount;
-        else if (diff > Kingdom.ExRateLow)
-            ret = Kingdom.ExRateMedAmount;
-        else if (diff < -Kingdom.ExRateMed)
-            ret = -Kingdom.ExRateHighAmount;
-        else if (diff < -Kingdom.ExRateLow)
-            ret = -Kingdom.ExRateMedAmount;
-        else
-            ret = Kingdom.ExRateLowAmount;
-
-        return ret;
+        return this.Resources.First(res => res.HomeResource);
     }
 }
--- BlazorSignalRApp.Client/Models/Resource.cs
+++ BlazorSignalRApp.Client/Models/Resource.cs
@@ -2,7 +2,45 @@ namespace BlazorSignalRApp.Client.Models;
 
 public class Resource
 {
+    public static int ExRateLowAmount = 1;
+    public static int ExRateLow = 10;
+    public static int ExRateMedAmount = 2;
+    public static int ExRateMed = 20;
+    public static int ExRateHighAmount = 3;
+    
+    public static int HomeResourceAmountFreeMoreThan = 30;
+    // ----
+    
     public string Name { get; set; }
     public int Amount { get; set; }
     public bool HomeResource { get; set; }
+    
+    // ----
+
+    public int GetExchangeRate(Resource targetResource)
+    {
+        return GetExchangeRate(this.Amount, targetResource.Amount);
+    }
+    
+    public static int GetExchangeRate(int offeredResourceAmount, int targetResourceAmount)
+    {
+        if (targetResourceAmount == 0)
+            return 0;
+        
+        var diff = targetResourceAmount - offeredResourceAmount;
+
+        int ret;
+        if (diff > ExRateMed)
+            ret = ExRateHighAmount;
+        else if (diff > ExRateLow)
+            ret = ExRateMedAmount;
+        else if (diff < -ExRateMed)
+            ret = -ExRateHighAmount;
+        else if (diff < -ExRateLow)
+            ret = -ExRateMedAmount;
+        else
+            ret = ExRateLowAmount;
+
+        return ret;
+    }
 }
